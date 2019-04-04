fa93127c679f973e8b1c8a061e207421ca79308c
codr7 <andreas.gone.wild@gmail.com>
2019-04-05 01:59:10 +0200

    drop checkargs

--- v1/src/gfu/prim.go
+++ v1/src/gfu/prim.go
@@ -16,17 +16,6 @@ func NewPrim(id *Sym, min_args, max_args int, imp PrimImp) *Prim {
   return p
 }
 
-func (p *Prim) CheckArgs(g *G, pos Pos, args []Val) E {
-  nargs := len(args)
-  
-  if (p.min_args != -1 && nargs < p.min_args) ||
-    (p.max_args != -1 && nargs > p.max_args) {
-    return g.E(pos, "Arg mismatch: %v", p.id)
-  }
-
-  return nil
-}
-
 func (e *Env) AddPrim(g *G, id *Sym, min_args, max_args int, imp PrimImp) {
   var p Val
   p.Init(g.Prim, NewPrim(id, min_args, max_args, imp))
