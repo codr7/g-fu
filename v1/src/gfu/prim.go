5aa5b532e6305c2bc266b3020c2c4946d12cb0b5
codr7 <andreas.gone.wild@gmail.com>
2019-04-05 02:08:10 +0200

    simplified addprim

--- v1/src/gfu/prim.go
+++ v1/src/gfu/prim.go
@@ -4,20 +4,19 @@ type PrimImp func (*G, Pos, VecForm, *Env) (Val, E)
 
 type Prim struct {
   id *Sym
-  min_args, max_args int
   imp PrimImp
 }
 
-func NewPrim(id *Sym, min_args, max_args int, imp PrimImp) *Prim {
+func NewPrim(id *Sym, imp PrimImp) *Prim {
   p := new(Prim)
   p.id = id
-  p.min_args, p.max_args = min_args, max_args
   p.imp = imp
   return p
 }
 
-func (e *Env) AddPrim(g *G, id *Sym, min_args, max_args int, imp PrimImp) {
+func (e *Env) AddPrim(g *G, id string, imp PrimImp) {
   var p Val
-  p.Init(g.Prim, NewPrim(id, min_args, max_args, imp))
-  e.Put(id, p)
+  ids := g.S(id)
+  p.Init(g.Prim, NewPrim(ids, imp))
+  e.Put(ids, p)
 }
