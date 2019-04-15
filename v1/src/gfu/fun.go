6275287636fcbdbd4ab75ff202383ecee004d807
codr7 <andreas.gone.wild@gmail.com>
2019-04-15 13:33:36 +0200

    renamed letvars

--- v1/src/gfu/fun.go
+++ v1/src/gfu/fun.go
@@ -1,125 +1,125 @@
 package gfu
 
 import (
-	"fmt"
-	//"log"
-	"strings"
+  "fmt"
+  //"log"
+  "strings"
 )
 
 type FunImp func(*G, *Task, *Env, Vec) (Val, E)
 
 type Fun struct {
-	env      *Env
-	arg_list ArgList
-	body     Vec
-	imp      FunImp
+  env      *Env
+  arg_list ArgList
+  body     Vec
+  imp      FunImp
 }
 
 func NewFun(g *G, env *Env, args []Arg) *Fun {
-	return new(Fun).Init(g, env, args)
+  return new(Fun).Init(g, env, args)
 }
 
 func (f *Fun) Init(g *G, env *Env, args []Arg) *Fun {
-	f.env = env
-	f.arg_list.Init(g, args)
-	return f
+  f.env = env
+  f.arg_list.Init(g, args)
+  return f
 }
 
 func (_ *Fun) Bool(g *G) bool {
-	return true
+  return true
 }
 
 func (f *Fun) Call(g *G, task *Task, env *Env, args Vec) (Val, E) {
-	avs, e := args.EvalVec(g, task, env)
+  avs, e := args.EvalVec(g, task, env)
 
-	if e != nil {
-		return nil, g.E("Args eval failed: %v", e)
-	}
+  if e != nil {
+    return nil, g.E("Args eval failed: %v", e)
+  }
 
-	if e := f.arg_list.Check(g, avs); e != nil {
-		return nil, e
-	}
+  if e := f.arg_list.Check(g, avs); e != nil {
+    return nil, e
+  }
 
-	if f.imp != nil {
-		return f.imp(g, task, env, f.arg_list.Fill(g, avs))
-	}
+  if f.imp != nil {
+    return f.imp(g, task, env, f.arg_list.Fill(g, avs))
+  }
 
-	var be Env
-	f.env.Clone(&be)
-	var v Val
+  var be Env
+  f.env.Clone(&be)
+  var v Val
 recall:
-	f.arg_list.LetEnv(g, &be, avs)
+  f.arg_list.LetVars(g, &be, avs)
 
-	if v, e = f.body.EvalExpr(g, task, &be); e != nil {
-		task.recall_args = nil
-		task.recall = false
-		return nil, e
-	}
+  if v, e = f.body.EvalExpr(g, task, &be); e != nil {
+    task.recall_args = nil
+    task.recall = false
+    return nil, e
+  }
 
-	if task.recall {
-		avs, task.recall_args = task.recall_args, nil
-		task.recall = false
-		goto recall
-	}
+  if task.recall {
+    avs, task.recall_args = task.recall_args, nil
+    task.recall = false
+    goto recall
+  }
 
-	return v, e
+  return v, e
 }
 
 func (f *Fun) Dump(out *strings.Builder) {
-	out.WriteString("(fun (")
+  out.WriteString("(fun (")
 
-	for i, a := range f.arg_list.items {
-		if i > 0 {
-			out.WriteRune(' ')
-		}
+  for i, a := range f.arg_list.items {
+    if i > 0 {
+      out.WriteRune(' ')
+    }
 
-		out.WriteString(a.id.name)
-	}
+    out.WriteString(a.id.name)
+  }
 
-	if f.imp == nil {
-		fmt.Fprintf(out, ") %v)", f.imp)
-	} else {
-		out.WriteString(") ")
+  if f.imp == nil {
+    fmt.Fprintf(out, ") %v)", f.imp)
+  } else {
+    out.WriteString(") ")
 
-		for i, bv := range f.body {
-			if i > 0 {
-				out.WriteRune(' ')
-			}
+    for i, bv := range f.body {
+      if i > 0 {
+        out.WriteRune(' ')
+      }
 
-			bv.Dump(out)
-		}
+      bv.Dump(out)
+    }
 
-		out.WriteRune(')')
-	}
+    out.WriteRune(')')
+  }
 }
 
 func (f *Fun) Eq(g *G, rhs Val) bool {
-	return f.Is(g, rhs)
+  return f.Is(g, rhs)
 }
 
 func (f *Fun) Eval(g *G, task *Task, env *Env) (Val, E) {
-	return f, nil
+  return f, nil
 }
 
 func (f *Fun) Is(g *G, rhs Val) bool {
-	return f == rhs
+  return f == rhs
 }
 
 func (f *Fun) Quote(g *G, task *Task, env *Env) (Val, E) {
-	return f, nil
+  return f, nil
 }
 
 func (f *Fun) Splat(g *G, out Vec) Vec {
-	return append(out, f)
+  return append(out, f)
 }
 
 func (f *Fun) Type(g *G) *Type {
-	return &g.FunType
+  return &g.FunType
 }
 
 func (env *Env) AddFun(g *G, id string, imp FunImp, args ...Arg) E {
-	f := NewFun(g, env, args)
-	f.imp = imp
-	env.Let(g.Sym(id), f)
-	return nil
+  f := NewFun(g, env, args)
+  f.imp = imp
+  env.Let(g.Sym(id), f)
+  return nil
 }
--- v1/src/gfu/macro.go
+++ v1/src/gfu/macro.go
@@ -1,98 +1,98 @@
 package gfu
 
 import (
-	//"log"
-	"strings"
+  //"log"
+  "strings"
 )
 
 type Macro struct {
-	env      *Env
-	arg_list ArgList
-	body     Vec
+  env      *Env
+  arg_list ArgList
+  body     Vec
 }
 
 func NewMacro(g *G, env *Env, args []Arg) *Macro {
-	return new(Macro).Init(g, env, args)
+  return new(Macro).Init(g, env, args)
 }
 
 func (m *Macro) Init(g *G, env *Env, args []Arg) *Macro {
-	m.env = env
-	m.arg_list.Init(g, args)
-	return m
+  m.env = env
+  m.arg_list.Init(g, args)
+  return m
 }
 
 func (m *Macro) Bool(g *G) bool {
-	return true
+  return true
 }
 
 func (m *Macro) Call(g *G, task *Task, env *Env, args Vec) (v Val, e E) {
-	avs := make(Vec, len(args))
+  avs := make(Vec, len(args))
 
-	for i, a := range args {
-		if avs[i], e = a.Quote(g, task, env); e != nil {
-			return nil, e
-		}
-	}
+  for i, a := range args {
+    if avs[i], e = a.Quote(g, task, env); e != nil {
+      return nil, e
+    }
+  }
 
-	if e = m.arg_list.Check(g, args); e != nil {
-		return nil, e
-	}
+  if e = m.arg_list.Check(g, args); e != nil {
+    return nil, e
+  }
 
-	var be Env
-	m.env.Clone(&be)
-	m.arg_list.LetEnv(g, &be, args)
+  var be Env
+  m.env.Clone(&be)
+  m.arg_list.LetVars(g, &be, args)
 
-	if v, e = m.body.EvalExpr(g, task, &be); e != nil {
-		return nil, e
-	}
+  if v, e = m.body.EvalExpr(g, task, &be); e != nil {
+    return nil, e
+  }
 
-	return v.Eval(g, task, env)
+  return v.Eval(g, task, env)
 }
 
 func (m *Macro) Dump(out *strings.Builder) {
-	out.WriteString("(macro (")
+  out.WriteString("(macro (")
 
-	for i, a := range m.arg_list.items {
-		if i > 0 {
-			out.WriteRune(' ')
-		}
+  for i, a := range m.arg_list.items {
+    if i > 0 {
+      out.WriteRune(' ')
+    }
 
-		out.WriteString(a.id.name)
-	}
+    out.WriteString(a.id.name)
+  }
 
-	out.WriteString(") ")
+  out.WriteString(") ")
 
-	for i, bv := range m.body {
-		if i > 0 {
-			out.WriteRune(' ')
-		}
+  for i, bv := range m.body {
+    if i > 0 {
+      out.WriteRune(' ')
+    }
 
-		bv.Dump(out)
-	}
+    bv.Dump(out)
+  }
 
-	out.WriteRune(')')
+  out.WriteRune(')')
 }
 
 func (m *Macro) Eq(g *G, rhs Val) bool {
-	return m == rhs
+  return m == rhs
 }
 
 func (m *Macro) Eval(g *G, task *Task, env *Env) (Val, E) {
-	return m, nil
+  return m, nil
 }
 
 func (m *Macro) Is(g *G, rhs Val) bool {
-	return m == rhs
+  return m == rhs
 }
 
 func (m *Macro) Quote(g *G, task *Task, env *Env) (Val, E) {
-	return m, nil
+  return m, nil
 }
 
 func (m *Macro) Splat(g *G, out Vec) Vec {
-	return append(out, m)
+  return append(out, m)
 }
 
 func (m *Macro) Type(g *G) *Type {
-	return &g.MacroType
+  return &g.MacroType
 }
