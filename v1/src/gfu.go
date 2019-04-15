6b13d180e8ccd63f1e23164bd8e665ccfcc769f0
codr7 <andreas.gone.wild@gmail.com>
2019-04-15 18:46:12 +0200

    removed debug

--- v1/src/gfu.go
+++ v1/src/gfu.go
@@ -1,79 +1,78 @@
 package main
 
 import (
-	"bufio"
-	"flag"
-	"fmt"
-	"log"
-	"os"
-	"runtime/pprof"
-	"strings"
+  "bufio"
+  "flag"
+  "fmt"
+  "log"
+  "os"
+  "runtime/pprof"
+  "strings"
 
-	"./gfu"
+  "./gfu"
 )
 
 var prof = flag.String("prof", "", "Write CPU profile to specified file")
 
 func main() {
-	g, e := gfu.NewG()
+  g, e := gfu.NewG()
 
-	if e != nil {
-		log.Fatal(e)
-	}
+  if e != nil {
+    log.Fatal(e)
+  }
 
-	//g.Debug = true
-	g.RootEnv.InitAbc(g)
-	flag.Parse()
+  g.RootEnv.InitAbc(g)
+  flag.Parse()
 
-	if *prof != "" {
-		f, e := os.Create(*prof)
+  if *prof != "" {
+    f, e := os.Create(*prof)
 
-		if e != nil {
-			log.Fatal(e)
-		}
+    if e != nil {
+      log.Fatal(e)
+    }
 
-		pprof.StartCPUProfile(f)
-		defer pprof.StopCPUProfile()
-	}
+    pprof.StartCPUProfile(f)
+    defer pprof.StopCPUProfile()
+  }
 
-	args := flag.Args()
+  args := flag.Args()
 
-	if len(args) == 0 {
-		fmt.Printf("g-fu v1.8\n\nPress Return twice to evaluate.\n\n  ")
-		in := bufio.NewScanner(os.Stdin)
-		var buf strings.Builder
+  if len(args) == 0 {
+    fmt.Printf("g-fu v1.8\n\nPress Return twice to evaluate.\n\n  ")
+    in := bufio.NewScanner(os.Stdin)
+    var buf strings.Builder
 
-		for in.Scan() {
-			line := in.Text()
+    for in.Scan() {
+      line := in.Text()
 
-			if len(line) == 0 {
-				if buf.Len() > 0 {
-					v, e := g.EvalString(&g.MainTask, g.NewEnv(), gfu.INIT_POS, buf.String())
+      if len(line) == 0 {
+        if buf.Len() > 0 {
+          v, e := g.EvalString(&g.MainTask, g.NewEnv(), gfu.INIT_POS, buf.String())
 
-					if e == nil {
-						fmt.Printf("\r%v\n", v)
-					} else {
-						fmt.Printf("\r%v\n", e)
-					}
-				}
+          if e == nil {
+            fmt.Printf("\r%v\n", v)
+          } else {
+            fmt.Printf("\r%v\n", e)
+          }
+        }
 
-				buf.Reset()
-			} else {
-				buf.WriteString(line)
-				buf.WriteRune('\n')
-			}
+        buf.Reset()
+      } else {
+        buf.WriteString(line)
+        buf.WriteRune('\n')
+      }
 
-			fmt.Printf("  ")
-		}
+      fmt.Printf("  ")
+    }
 
-		if e := in.Err(); e != nil {
-			log.Fatal(e)
-		}
-	} else {
-		for _, a := range args {
-			if _, e := g.Load(&g.MainTask, g.NewEnv(), a); e != nil {
-				log.Fatal(e)
-			}
-		}
-	}
+    if e := in.Err(); e != nil {
+      log.Fatal(e)
+    }
+  } else {
+    for _, a := range args {
+      if _, e := g.Load(&g.MainTask, g.NewEnv(), a); e != nil {
+        log.Fatal(e)
+      }
+    }
+  }
 }
