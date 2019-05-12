2bc5027c6928560ee0727f1c799291742ed185aa
codr7 <andreas.gone.wild@gmail.com>
2019-05-12 13:56:52 +0200

    error fmt

--- v1/src/gfu/arg.go
+++ v1/src/gfu/arg.go
@@ -1,206 +1,223 @@
 package gfu
 
 import (
-	//"log"
-	"strings"
+  //"log"
+  "strings"
 )
 
 type ArgType int
 
 const (
-	ARG_PLAIN ArgType = 0
-	ARG_OPT   ArgType = 1
-	ARG_SPLAT ArgType = 2
+  ARG_PLAIN ArgType = 0
+  ARG_OPT   ArgType = 1
+  ARG_SPLAT ArgType = 2
 )
 
 type Arg struct {
-	arg_type ArgType
-	str_id   string
-	id       *Sym
-	opt_val  Val
+  arg_type ArgType
+  str_id   string
+  id       *Sym
+  opt_val  Val
 }
 
 func (a *Arg) Init(id *Sym) *Arg {
-	a.id = id
-	return a
+  a.id = id
+  return a
 }
 
 func A(id string) (a Arg) {
-	a.str_id = id
-	return a
+  a.str_id = id
+  return a
 }
 
 func AOpt(id string, val Val) (a Arg) {
-	a.str_id = id
-	a.opt_val = val
-	a.arg_type = ARG_OPT
-	return a
+  a.str_id = id
+  a.opt_val = val
+  a.arg_type = ARG_OPT
+  return a
 }
 
 func ASplat(id string) (a Arg) {
-	a.str_id = id
-	a.arg_type = ARG_SPLAT
-	return a
+  a.str_id = id
+  a.arg_type = ARG_SPLAT
+  return a
 }
 
 func (a Arg) Dump(g *G, out *strings.Builder) E {
-	switch a.arg_type {
-	case ARG_OPT:
-		out.WriteRune('(')
-		out.WriteString(a.id.name)
+  switch a.arg_type {
+  case ARG_OPT:
+    out.WriteRune('(')
+    out.WriteString(a.id.name)
 
-		if a.opt_val != nil {
-			out.WriteRune(' ')
+    if a.opt_val != nil {
+      out.WriteRune(' ')
 
-			if e := g.Dump(a.opt_val, out); e != nil {
-				return e
-			}
-		}
+      if e := g.Dump(a.opt_val, out); e != nil {
+        return e
+      }
+    }
 
-		out.WriteRune(')')
-	case ARG_SPLAT:
-		out.WriteString(a.id.name)
-		out.WriteString("..")
-	default:
-		out.WriteString(a.id.name)
-	}
+    out.WriteRune(')')
+  case ARG_SPLAT:
+    out.WriteString(a.id.name)
+    out.WriteString("..")
+  default:
+    out.WriteString(a.id.name)
+  }
 
-	return nil
+  return nil
+}
+
+type Args []Arg
+
+func (as Args) Dump(g *G, out *strings.Builder) E {
+  out.WriteRune('(')
+
+  for i, a := range as {
+    if i > 0 {
+      out.WriteRune(' ')
+    }
+    
+    a.Dump(g, out)
+  }
+  
+  out.WriteRune(')')
+  return nil
 }
 
 type ArgList struct {
-	items    []Arg
-	min, max int
+  items    Args
+  min, max int
 }
 
-func (l *ArgList) Init(g *G, args []Arg) *ArgList {
-	nargs := len(args)
+func (l *ArgList) Init(g *G, args Args) *ArgList {
+  nargs := len(args)
 
-	if nargs == 0 {
-		return l
-	}
+  if nargs == 0 {
+    return l
+  }
 
-	l.items = args
-	l.min, l.max = nargs, nargs
+  l.items = args
+  l.min, l.max = nargs, nargs
 
-	for i, a := range l.items {
-		if a.arg_type == ARG_OPT {
-			l.min--
-		}
+  for i, a := range l.items {
+    if a.arg_type == ARG_OPT {
+      l.min--
+    }
 
-		if a.id == nil {
-			l.items[i].id = g.Sym(a.str_id)
-		}
-	}
+    if a.id == nil {
+      l.items[i].id = g.Sym(a.str_id)
+    }
+  }
 
-	a := l.items[nargs-1]
+  a := l.items[nargs-1]
 
-	if a.arg_type == ARG_SPLAT {
-		l.min--
-		l.max = -1
-	}
+  if a.arg_type == ARG_SPLAT {
+    l.min--
+    l.max = -1
+  }
 
-	return l
+  return l
 }
 
 func (l *ArgList) Check(g *G, args Vec) E {
-	nargs := len(args)
+  nargs := len(args)
 
-	if (l.min != -1 && nargs < l.min) || (l.max != -1 && nargs > l.max) {
-		return g.E("Arg mismatch: %v %v", l.items, args)
-	}
+  if (l.min != -1 && nargs < l.min) || (l.max != -1 && nargs > l.max) {
+    return g.E("Arg mismatch: %v %v", l.items, args)
+  }
 
-	return nil
+  return nil
 }
 
 func (l *ArgList) Fill(g *G, args Vec) Vec {
-	for i := len(args); i < len(l.items); i++ {
-		a := l.items[i]
+  for i := len(args); i < len(l.items); i++ {
+    a := l.items[i]
 
-		if a.arg_type != ARG_OPT {
-			break
-		}
+    if a.arg_type != ARG_OPT {
+      break
+    }
 
-		args = append(args, a.OptVal(g))
-	}
+    args = append(args, a.OptVal(g))
+  }
 
-	return args
+  return args
 }
 
 func (a Arg) OptVal(g *G) Val {
-	v := a.opt_val
+  v := a.opt_val
 
-	if v == nil {
-		v = &g.NIL
-	}
+  if v == nil {
+    v = &g.NIL
+  }
 
-	return v
+  return v
 }
 
 func (l *ArgList) LetVars(g *G, env *Env, args Vec) E {
-	nargs := len(args)
+  nargs := len(args)
 
-	for i, a := range l.items {
-		if a.arg_type == ARG_SPLAT {
-			var v Vec
+  for i, a := range l.items {
+    if a.arg_type == ARG_SPLAT {
+      var v Vec
 
-			if i < nargs {
-				v = make(Vec, nargs-i)
-				copy(v, args[i:])
-			}
+      if i < nargs {
+        v = make(Vec, nargs-i)
+        copy(v, args[i:])
+      }
 
-			if e := env.Let(g, a.id, v); e != nil {
-				return e
-			}
+      if e := env.Let(g, a.id, v); e != nil {
+        return e
+      }
 
-			break
-		}
+      break
+    }
 
-		var v Val
+    var v Val
 
-		if i < nargs {
-			v = args[i]
-		} else {
-			v = a.OptVal(g)
-		}
+    if i < nargs {
+      v = args[i]
+    } else {
+      v = a.OptVal(g)
+    }
 
-		if e := env.Let(g, a.id, v); e != nil {
-			return e
-		}
-	}
+    if e := env.Let(g, a.id, v); e != nil {
+      return e
+    }
+  }
 
-	return nil
+  return nil
 }
 
-func ParseArgs(g *G, task *Task, env *Env, in Vec) ([]Arg, E) {
-	var e E
-	var out []Arg
+func ParseArgs(g *G, task *Task, env *Env, in Vec) (Args, E) {
+  var e E
+  var out Args
 
-	for _, v := range in {
-		var a Arg
+  for _, v := range in {
+    var a Arg
 
-		if id, ok := v.(*Sym); ok {
-			a.id = id
-		} else if vv, ok := v.(Vec); ok {
-			if len(vv) < 2 {
-				return nil, g.E("Invalid arg: %v", vv)
-			}
+    if id, ok := v.(*Sym); ok {
+      a.id = id
+    } else if vv, ok := v.(Vec); ok {
+      if len(vv) < 2 {
+        return nil, g.E("Invalid arg: %v", vv)
+      }
 
-			a.arg_type = ARG_OPT
-			a.id = vv[0].(*Sym)
+      a.arg_type = ARG_OPT
+      a.id = vv[0].(*Sym)
 
-			if a.opt_val, e = g.Eval(task, env, vv[1]); e != nil {
-				return nil, e
-			}
-		} else if sv, ok := v.(Splat); ok {
-			a.arg_type = ARG_SPLAT
-			a.id = sv.val.(*Sym)
-		} else {
-			return nil, g.E("Invalid arg: %v", v)
-		}
+      if a.opt_val, e = g.Eval(task, env, vv[1]); e != nil {
+        return nil, e
+      }
+    } else if sv, ok := v.(Splat); ok {
+      a.arg_type = ARG_SPLAT
+      a.id = sv.val.(*Sym)
+    } else {
+      return nil, g.E("Invalid arg: %v", v)
+    }
 
-		out = append(out, a)
-	}
+    out = append(out, a)
+  }
 
-	return out, nil
+  return out, nil
 }
--- v1/src/gfu/dump.go
+++ v1/src/gfu/dump.go
@@ -1,15 +1,19 @@
 package gfu
 
 import (
-	"strings"
+  "strings"
 )
 
 type Dumper interface {
-	Dump(*strings.Builder)
+  Dump(*G, *strings.Builder) E
 }
 
-func DumpString(d Dumper) string {
-	var out strings.Builder
-	d.Dump(&out)
-	return out.String()
+func DumpString(g *G, d Dumper) (string, E) {
+  var out strings.Builder
+
+  if e := d.Dump(g, &out); e != nil {
+    return "", e
+  }
+  
+  return out.String(), nil
 }
