e6666fcdaff86fc40e39197aff54de998817f245
codr7 <andreas.gone.wild@gmail.com>
2019-05-19 20:43:27 +0200

    todo

--- v1/src/gfu/str.go
+++ v1/src/gfu/str.go
@@ -1,65 +1,75 @@
 package gfu
 
 import (
-	//"log"
-	"strings"
+  //"log"
+  "strings"
 )
 
 type Str []rune
 
 type StrType struct {
-	BasicType
+  BasicType
 }
 
 func (s Str) Len() Int {
-	return Int(len(s))
+  return Int(len(s))
 }
 
 func (_ Str) Type(g *G) Type {
-	return &g.StrType
+  return &g.StrType
 }
 
 func (_ *StrType) Bool(g *G, val Val) (bool, E) {
-	return val.(Str).Len() > 0, nil
+  return val.(Str).Len() > 0, nil
 }
 
 func (_ *StrType) Drop(g *G, val Val, n Int) (Val, E) {
-	s := val.(Str)
-	sl := s.Len()
+  s := val.(Str)
+  sl := s.Len()
 
-	if sl < n {
-		return nil, g.E("Nothing to drop")
-	}
+  if sl < n {
+    return nil, g.E("Nothing to drop")
+  }
 
-	return s[:sl-n], nil
+  return s[:sl-n], nil
 }
 
 func (_ *StrType) Dump(g *G, val Val, out *strings.Builder) E {
-	out.WriteRune('"')
-	out.WriteString(string(val.(Str)))
-	out.WriteRune('"')
-	return nil
+  out.WriteRune('"')
+  out.WriteString(string(val.(Str)))
+  out.WriteRune('"')
+  return nil
 }
 
 func (_ *StrType) Eq(g *G, lhs, rhs Val) (bool, E) {
-	rs, ok := rhs.(Str)
-	return ok && string(lhs.(Str)) == string(rs), nil
+  rs, ok := rhs.(Str)
+  return ok && string(lhs.(Str)) == string(rs), nil
 }
 
 func (_ *StrType) Len(g *G, val Val) (Int, E) {
-	return val.(Str).Len(), nil
+  return val.(Str).Len(), nil
 }
 
 func (_ *StrType) Print(g *G, val Val, out *strings.Builder) {
-	out.WriteString(string(val.(Str)))
+  out.WriteString(string(val.(Str)))
 }
 
 func (g *G) String(val Val) (string, E) {
-	var out strings.Builder
+  var out strings.Builder
 
-	if e := g.Dump(val, &out); e != nil {
-		return "", e
-	}
+  if e := g.Dump(val, &out); e != nil {
+    return "", e
+  }
 
-	return out.String(), nil
+  return out.String(), nil
+}
+
+func (g *G) EString(val Val) string {
+  s, e := g.String(val)
+
+  if e != nil {
+    s = e.String()
+  }
+
+  return s
 }
